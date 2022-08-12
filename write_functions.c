cfa2a7e9449e66a91f378c1e551abcea98a276a1
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-08-12 02:20:52 -0700

    t0

--- write_functions.c
+++ write_functions.c
@@ -9,10 +9,10 @@
  */
 int _putchar(char c)
 {
-	static char buf[200];
+	static char buf[1024];
 	static int i;
 
-	if (c == -1 || i >= 200)
+	if (c == -1 || i >= 1024)
 	{
 		write(1, &buf, i);
 		i = 0;
