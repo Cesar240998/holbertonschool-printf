cba4ef66b51be310fceb61d11f94919b07757e64
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-08-12 11:43:25 -0700

    header1

--- main.h
+++ main.h
@@ -6,38 +6,38 @@
 #include <unistd.h>
 
 /**
- * struct special_chars - Validate the special char (%?)
- * @op: A char with some option
- * @operation: Pointer to the function that go to print
- */
-
-typedef struct special_chars
-{
-	int op;
-	int (*operation)(va_list args);
-} special_chars_t;
-
-/**
- * struct print_operation - Contain and call the function to print
+ * struct validates - Contain and call the function to print
  * @flag: A flag to validate if exist a special char (%?).
  * @print: function to print something.
  */
 
-typedef struct print_operation
+typedef struct validates
 {
-	int flag;
-	int (*print)(va_list args);
-} operation_t;
+        int flag;
+        int (*print)(va_list args);
+} validates_t;
 
 int print_int(va_list args);
 void print_number(int n);
 int count_digit(int i);
 int _printf(const char *format, ...);
-operation_t ch_option(char s);
+validates_t ch_option(char s);
 int print_string(va_list args);
 int print_char(va_list args);
 int print_percent(va_list args __attribute__((unused)));
 
 int _putchar(char c);
+
+/**
+ * struct specifiers - gives the specifiers with their funcs
+ * @op: A char with some option
+ * @oper: Pointer to the function that go to print
+ */
+
+typedef struct specifiers
+{
+        int op;
+        int (*oper)(va_list args);
+} specifiers_t;
 
 #endif
